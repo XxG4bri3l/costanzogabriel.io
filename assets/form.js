17e0ace9c935c35c404b89b6945aa8d51afa211a
XxG4bri3l <141683016+XxG4bri3l@users.noreply.github.com>
2023-08-13 16:16:50 +0200

    Add files via upload

--- assets/form.js
+++ assets/form.js
@@ -1,13 +1,22 @@
-// script.js
-function Sendmail() {
-  var params= {
-    email_id : document.getElementById("email_id").value,
-    message : document.getElementById("message").value,
-
-
+function sendMail() {
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
+    };
+  
+    const serviceID = "service_uv3nr7t";
+    const templateID = "template_aa35hcp";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          alert("Your message sent successfully!!")
+  
+      })
+      .catch(err=>console.log(err));
+  
   }
-  emailjs.send("service_uv3nr7t", "template_eu45r6r", params).then(function(res{
-    alert("Success" + res.status)
-  }
-    )
-}
