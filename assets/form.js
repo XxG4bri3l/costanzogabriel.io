b8b79278749f8ba2dfe242088740c3f4da645351
XxG4bri3l <141683016+XxG4bri3l@users.noreply.github.com>
2023-08-18 11:36:08 +0200

    Update form.js

--- assets/form.js
+++ assets/form.js
@@ -22,20 +22,7 @@ function sendMail()
   
   }
 
-function validation(){
-var name = document.getElementById("name").value;
-var email = document.getElementById("email").value;
-var emailReg = /^([w-.]+@([w-]+.)+[w-]{2,4})?$/;
-if( name ==='' || email ===''){
-alert("Please fill all fields...!!!!!!");
-return false;
-}else if(!(email).match(emailReg)){
-alert("Invalid Email...!!!!!!");
-return false;
-}else{
-return true;
-}
-}
+
 
   
 
